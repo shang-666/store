6ab8237d3a14091204e470343c9987a99daf538a
shang-666 <73047765+shang-666@users.noreply.github.com>
2021-11-24 09:38:41 +0800

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,26 @@
+# import random
+# qis=5000
+# i=0
+# while True:
+#     ran=random.randint(1,10)
+#     num=int(input("请输入一个金额"))
+#     if num==ran:
+#         print("猜对了")
+#         qis=qis+300
+#
+#     else:
+#         if num>ran :
+#             print ("大了")
+#         if num<ran :
+#             print("小了")
+#
+#         qis=qis-100
+#         i=i+1
+#         if i==15:
+#             break
+#     print("当前金额为",qis)
+#     continue
+
+
+for i in range(1,9):
+    print(i)
